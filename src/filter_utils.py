bb9c436401f816e4e07050e8be08903d173e1079
Wok <woctezuma@users.noreply.github.com>
2021-12-19 11:30:27 +0100

    Generalize filters, to tackle current or upcoming promos

--- src/filter_utils.py
+++ src/filter_utils.py
@@ -1,14 +1,32 @@
-def has_upcoming_promo(element):
+def get_promo_str(check_upcoming_promos):
+    if check_upcoming_promos:
+        promo_str = "upcomingPromotionalOffers"
+    else:
+        promo_str = "promotionalOffers"
+    return promo_str
+
+
+def is_relevant_promo(element, check_upcoming_promos):
+    promo_str = get_promo_str(check_upcoming_promos)
     flag = (
-            element["promotions"] is not None
-            and len(element["promotions"]["upcomingPromotionalOffers"]) > 0
+        element["promotions"] is not None
+        and promo_str in element["promotions"]
+        and len(element["promotions"][promo_str]) > 0
     )
 
     return flag
 
 
-def filter_promos(promos):
-    filtered_promos = [e for e in promos if has_upcoming_promo(e)]
+def has_current_promo(element):
+    return is_relevant_promo(element, check_upcoming_promos=False)
+
+
+def has_upcoming_promo(element):
+    return is_relevant_promo(element, check_upcoming_promos=True)
+
+
+def filter_promos(promos, check_upcoming_promos=True):
+    filtered_promos = [e for e in promos if is_relevant_promo(e, check_upcoming_promos)]
 
     return filtered_promos
 

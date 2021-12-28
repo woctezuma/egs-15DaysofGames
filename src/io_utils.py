a5d3ed0c4ef37083c6838ba06159dd6729d37044
Wok <woctezuma@users.noreply.github.com>
2021-12-28 14:12:01 +0100

    Add functions to load Auth Clients from JSON

--- src/io_utils.py
+++ src/io_utils.py
@@ -26,6 +26,20 @@ def save_promos(data: list[dict], fname: str = None) -> None:
     return
 
 
+def get_auth_client_filename() -> str:
+    fname = "data/auth_clients.json"
+    return fname
+
+
+def load_auth_clients() -> list[dict[str, str | None]]:
+    fname = get_auth_client_filename()
+
+    with open(fname, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    return data
+
+
 if __name__ == "__main__":
     fname = get_output_filename()
     print(fname)

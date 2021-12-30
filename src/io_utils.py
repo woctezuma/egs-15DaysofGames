033aa5a7c71cc20afc34c356726f9c9e489ef3c3
Wok <woctezuma@users.noreply.github.com>
2021-12-30 20:17:48 +0100

    Add utils to load cookie info from disk

--- src/io_utils.py
+++ src/io_utils.py
@@ -57,6 +57,25 @@ def load_target_auth_client(
     return first_client
 
 
+def get_epic_cookie_file_name() -> str:
+    epic_cookie_file_name = "data/personal_info.json"
+
+    return epic_cookie_file_name
+
+
+def load_epic_cookie_from_disk(fname: str = None) -> dict[str, str]:
+    if fname is None:
+        fname = get_epic_cookie_file_name()
+
+    try:
+        with open(fname, "r", encoding="utf-8") as f:
+            cookie = json.load(f)
+    except FileNotFoundError:
+        cookie = dict()
+
+    return cookie
+
+
 if __name__ == "__main__":
     fname = get_output_filename()
     print(fname)

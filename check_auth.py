8e3f9e59a0e74c4deb0bc4fe22fef2d52e0d5e3e
Wok <woctezuma@users.noreply.github.com>
2021-12-30 20:45:38 +0100

    Try to ask for an auth code in check_auth.py
    The auth code would be an empty string if:
    - either there was no cookie file found on the disk,
    - or the auth code returned by EGS was None.

--- check_auth.py
+++ check_auth.py
@@ -1,8 +1,9 @@
+from src.auth_code_utils import get_authorization_code
 from src.auth_utils import query_graphql_while_auth
 
 if __name__ == "__main__":
     target_client_name = "launcherAppClient2"
-    authorization_code = ""
+    authorization_code = get_authorization_code()
 
     data = query_graphql_while_auth(
         target_client_name=target_client_name, authorization_code=authorization_code

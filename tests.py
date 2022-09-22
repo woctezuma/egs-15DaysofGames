4b6798107416c62c7e47ef8dae69dc30af6548ec
Wok <woctezuma@users.noreply.github.com>
2022-09-22 10:55:29 +0200

    Increase the expected access token length (1075->1112)

--- tests.py
+++ tests.py
@@ -499,7 +499,7 @@ class TestAuthTokenUtilsMethods(TestCase):
                 self.assertIsNotNone(access_token)
                 self.assertGreater(len(access_token), 0)
                 if ask_for_long_token:
-                    expected_access_token_length = 1075
+                    expected_access_token_length = 1112
                 else:
                     expected_access_token_length = 32
                 self.assertEqual(len(access_token), expected_access_token_length)
